98eaa047b9308039bccf285fc60c9ae114854ee1
zivserphos <zivfromisrael@gmail.com>
2025-05-06 00:56:40 +0300

    a

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config//
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
